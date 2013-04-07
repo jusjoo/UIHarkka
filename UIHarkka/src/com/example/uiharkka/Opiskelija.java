d657ffb987419644a8a7031c048b6b0daee73e2b
Valtteri <Valtteri@86.50.87.190>
2013-04-07 15:15:09 +0300

    opiskelija-luokka

---
+++ UIHarkka/src/com/example/uiharkka/Opiskelija.java
@@ -0,0 +1,18 @@
+package com.example.uiharkka;
+
+public class Opiskelija {
+
+	private String nimi;
+	private int opNum;
+	private int aloitusVuosi;
+	private int nopat;
+	private String koulutusOhj;
+	private String paaAine;
+
+	// private List<Suoritus> suoritetutKurssit;
+
+	public Opiskelija(String nimi, int opNum, int aloitusVuosi, int nopat,
+			String koulutusOhj, String paaAine) {
+
+	}
+}
