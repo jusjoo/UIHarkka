a84b64cb662d25f50dfd843cf7c9d2eb5e48009e
Valtteri <Valtteri@86.50.87.190>
2013-04-07 17:56:57 +0300

    ffd

--- UIHarkka/src/com/example/uiharkka/Kurssi.java
+++ UIHarkka/src/com/example/uiharkka/Kurssi.java
@@ -2,12 +2,19 @@ package com.example.uiharkka;
 
 public class Kurssi {
 
-	private int kurssiID;
-	private String kurssinNimi;
-	private int opintopisteet;
+	private final int kurssiID;
+	private final String kurssinNimi;
+	private final int opintopisteet;
 
 	public Kurssi(int kurssiID, String kurssinNimi, int opintopisteet) {
-
+		this.kurssiID = kurssiID;
+		this.kurssinNimi = kurssinNimi;
+		this.opintopisteet = opintopisteet;
 	}
 
+	@Override
+	public String toString() {
+		return "Kurssin nimi: " + kurssinNimi + ", Kurssin ID: " + kurssiID
+				+ ", Kurssin laajuus:" + opintopisteet + "op";
+	}
 }
--- UIHarkka/src/com/example/uiharkka/Opiskelija.java
+++ UIHarkka/src/com/example/uiharkka/Opiskelija.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 public class Opiskelija {
 
-	private int opNum;
-	private int nopat;
-	private String aloitusVuosi;
-	private String nimi;
-	private String koulutusOhj;
-	private String paaAine;
+	private final int opNum;
+	private final int nopat;
+	private final String aloitusvuosi;
+	private final String nimi;
+	private final String koulutusohj;
+	private final String paaAine;
 	private List<Suoritus> suoritetutKurssit;
 
-	public Opiskelija(String nimi, int opNum, String aloitusVuosi, int nopat,
-			String koulutusOhj, String paaAine) {
+	public Opiskelija(String nimi, int opNum, String aloitusvuosi, int nopat,
+			String koulutusohj, String paaAine) {
+		this.nimi = nimi;
+		this.opNum = opNum;
+		this.aloitusvuosi = aloitusvuosi;
+		this.nopat = nopat;
+		this.koulutusohj = koulutusohj;
+		this.paaAine = paaAine;
 
 	}
 
@@ -23,6 +29,14 @@ public class Opiskelija {
 
 	public int getOpNum() {
 		return opNum;
+	}
+
+	@Override
+	public String toString() {
+		return "Nimi: " + nimi + ", Opiskelijanumero: " + opNum
+				+ ", Opintopisteet: " + nopat + ", Aloitusvuosi:"
+				+ aloitusvuosi + ", Koulutusohjelma; " + koulutusohj
+				+ ", Pääaine: " + paaAine;
 	}
 
 }
