8f51f86033140907045e25f5dacc119d2cff1e49
Valtteri <Valtteri@86.50.87.190>
2013-04-07 16:34:55 +0300

    luokkia

--- UIHarkka/src/com/example/uiharkka/Opiskelija.java
+++ UIHarkka/src/com/example/uiharkka/Opiskelija.java
@@ -1,18 +1,28 @@
 package com.example.uiharkka;
 
+import java.util.List;
+
 public class Opiskelija {
 
-	private String nimi;
 	private int opNum;
-	private int aloitusVuosi;
 	private int nopat;
+	private String aloitusVuosi;
+	private String nimi;
 	private String koulutusOhj;
 	private String paaAine;
-
-	// private List<Suoritus> suoritetutKurssit;
+	private List<Suoritus> suoritetutKurssit;
 
 	public Opiskelija(String nimi, int opNum, int aloitusVuosi, int nopat,
 			String koulutusOhj, String paaAine) {
 
 	}
+
+	public void lisaaSuoritus(Suoritus suoritus) {
+		suoritetutKurssit.add(suoritus);
+	}
+
+	public int getOpNum() {
+		return opNum;
+	}
+
 }
